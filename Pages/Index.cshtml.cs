e2e1cda0145ddf0557f774e9e8a0f1a31cf236f1
lucasmenchon <it.lucas@outlook.com>
2022-07-23 02:51:45 -0300

    clear cache

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,16 +2,16 @@
 using Microsoft.Extensions.Logging;
 
 namespace webapp.Pages
-{   
+{
     public class HomeModel : PageModel
-    {    
+    {
         private readonly ILogger<HomeModel> _logger;
-             
+
         public HomeModel(ILogger<HomeModel> logger)
         {
             _logger = logger;
         }
-          
+
         public void OnGet()
         {
 
