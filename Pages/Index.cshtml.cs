7d128cc2a3d6c2aebb53e54df3aeacd6e63a83be
lucasmenchon <it.lucas@outlook.com>
2022-07-23 02:50:29 -0300

    update experiences, biography, bug fix

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace webapp.Pages
-{
+{   
     public class HomeModel : PageModel
-    {
+    {    
         private readonly ILogger<HomeModel> _logger;
-
+             
         public HomeModel(ILogger<HomeModel> logger)
         {
             _logger = logger;
         }
-
+          
         public void OnGet()
         {
 
